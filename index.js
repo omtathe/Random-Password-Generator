ced7389280288346a0df0369dc8801ca5fc35a35
omtathe <omtathe45@gmail.com>
2026-01-27 10:13:09 +0530

    Updated UI and fixed bugs

---
+++ index.js
@@ -0,0 +1,11 @@
+const btnEl = document.querySelector(".btn")
+
+btnEl.addEventListener("click",()=>
+{
+    createPassword()
+})
+
+function createPassword(){
+    const chars = "0123456789abcdefghijklmnopqrstuvwxtz!@#$%^&*()_+?:{}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    const passwordLength 14;
+}
