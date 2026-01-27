3e5e4ca4e9d0ec63f50389d014c65fa1dcd063df
omtathe <omtathe45@gmail.com>
2026-01-27 11:37:22 +0530

    Updated UI and fixed bugs

--- index.js
+++ index.js
@@ -7,5 +7,11 @@ btnEl.addEventListener("click",()=>
 
 function createPassword(){
     const chars = "0123456789abcdefghijklmnopqrstuvwxtz!@#$%^&*()_+?:{}[]ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    const passwordLength 14;
+    const passwordLength = 14;
+    let password = ""
+    for (let index = 0; index < passwordLength; index++) {
+        const randomNum = Math.floor(Math.random() * chars.length);
+        password += chars.substring(randomNum,randomNum + 1);
+        console.log(randomNum, password);
+    }
 }
